76acf2b3b1013a6f401cb184a9d47183488b0211
suntied <49231309+suntied@users.noreply.github.com>
2021-03-29 15:32:15 +0200

    users

---
+++ webAPI/mySearchEngine/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+#Dependancy for JWT Views
+from rest_framework_simplejwt import views as jwt_views
+
+#ENDPOINT JWT
+urlpatterns = [
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')] 
